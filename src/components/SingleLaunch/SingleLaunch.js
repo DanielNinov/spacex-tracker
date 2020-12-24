3393ad893990c8c9ee7aa1c28927cabd3d337c77
Daniel Ninov <daniel.ninov.a20@learn.telerikacademy.com>
2020-12-25 01:27:56 +0200

    Added social media icons and fixed single launch design

--- src/components/SingleLaunch/SingleLaunch.js
+++ src/components/SingleLaunch/SingleLaunch.js
@@ -1,26 +1,40 @@
 import React from 'react';
+import { Card, Col, Image, Row } from 'react-bootstrap';
+import { NO_PATCH } from '../../common/constants';
 import DateFormatter from '../../utils/DateFormatter';
 
 const SingleLaunch = ({ launch }) => {
   return (
     <>
-      <div className="card mb-3">
-        <div className="row no-gutters">
-          <div className="col-auto">
+      <Card className="mb-3">
+        <Row noGutters="true">
+          <Col className="col-auto">
             {launch.links.patch.small ?
-              <> <img className="launch-patch-small" src={launch.links.patch.small} alt={`${launch.name} patch`}
-                width="100" height="100" /> <br />
-              </> : null}
-          </div>
-          <div className="col">
-            <div className="card-body">
-              <h4 className="card-title">{launch.name}</h4>
-              <h6 className="card-subtitle mb-2">{DateFormatter(launch.date_utc, launch.date_precision, launch.tbd, launch.net)}</h6>
-              <p className="card-text">{launch.details}</p>
-            </div>
-          </div>
-        </div>
-      </div>
+              <Image className="launch-patch-small ml-1" src={launch.links.patch.small} alt={`${launch.name} patch`}
+                width="100" height="100" /> :
+              <Image className="launch-patch-small ml-1" src={NO_PATCH} alt={`${launch.name} missing patch`}
+                width="100" height="100" />} <br />
+          </Col>
+
+          <Col>
+            <Card.Body>
+              <Card.Title>{launch.name} / {launch.rocket.name}</Card.Title>
+              <Card.Subtitle className="mb-1">{DateFormatter(launch.date_utc, launch.date_precision, launch.tbd, launch.net)}</Card.Subtitle>
+
+              {launch.details ?
+              <Card.Text>{launch.details}</Card.Text> :
+              <Card.Text className="card-text">No description available</Card.Text>}
+
+              <Card.Link className="social-media-icon"
+                href={`${launch.links.reddit.campaign}`}
+                aria-label={`${launch.name} Reddit Link`}><i className="fab fa-reddit fa-lg" /></Card.Link>
+              <Card.Link className="social-media-icon"
+                href={`https://youtu.be/${launch.links.youtube_id}`}
+                aria-label={`${launch.name} Youtube Link`}><i className="fab fa-youtube fa-lg" /></Card.Link>
+            </Card.Body>
+          </Col>
+        </Row>
+      </Card>
     </>
   );
 }
