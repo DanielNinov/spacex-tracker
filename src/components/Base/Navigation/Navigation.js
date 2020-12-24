f607ed8a5f976a4292c3b8a0f8e72780010fc875
Daniel Ninov <daniel.ninov.a20@learn.telerikacademy.com>
2020-12-24 23:27:14 +0200

    Fixed navbar

--- src/components/Base/Navigation/Navigation.js
+++ src/components/Base/Navigation/Navigation.js
@@ -5,16 +5,19 @@ import { NavLink } from 'react-router-dom';
 
 const Navigation = () => {
   return (
-    <Navbar className="navbar navbar-expand-lg navbar-dark bg-dark">
-      <NavLink className="navbar-brand" to="/">SpaceX Launches Tracker</NavLink>
-      <Navbar.Toggle aria-controls="basic-navbar-nav" />
-      <Navbar.Collapse id="basic-navbar-nav">
-        <Nav className="mr-auto">
-          <NavLink className="nav-link" to="/upcoming">Upcoming Launches</NavLink>
-          <NavLink className="nav-link" to="/past">Past Launches</NavLink>
-        </Nav>
-      </Navbar.Collapse>
-    </Navbar>
+    <>
+      <Navbar collapseOnSelect expand="sm" fixed="top" bg="dark" variant="dark">
+        <Navbar.Brand to="/">SpaceX Launches Tracker</Navbar.Brand>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" label="Toggle navigation"/>
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <Nav className="mr-auto">
+            <NavLink className="nav-link" to="/upcoming">Upcoming Launches</NavLink>
+            <NavLink className="nav-link" to="/past">Past Launches</NavLink>
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
+      <div className="mb-5"></div>
+    </>
   );
 }
 
